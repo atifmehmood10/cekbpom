f0873303be2d39745b4ec63f4c07a35f59e30513
Muhammad Atif <atifmehmood010@gmail.com>
2021-05-02 01:52:11 +0500

    Add files via upload
    Update DB Helper
    Output JSON file to add records to db

--- companieshouse/Helpers/dbhelper.py
+++ companieshouse/Helpers/dbhelper.py
@@ -1,10 +1,39 @@
-import pyodbc
+import json
+import logging
+from datetime import datetime
 
-conn = pyodbc.connect('Driver={SQL Server Native Client 11.0};'
-                      'Server=localhost;'
-                      'Database=master;'
-                      'Trusted_Connection=yes;')
-cursor = conn.cursor()
-cursor.execute('')
-for row in cursor:
-    print(row)
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",
+                               user="root",
+                               password="",
+                               database="cekbpom")
+mycursor = mydb.cursor()
+
+def readJson(file_path):
+
+    f = open(file_path)
+    data = json.load(f)
+    sql = "INSERT into companieshouse (Nomor_Registrasi, Tanggal_Terbit, Diterbitkan_Oleh, Produk, " \
+          "Nama_Produk, Merk, Kemasan, Pendaftar) " \
+          "VALUES(%s, %s, %s, %s ,%s, %s, %s, %s)"
+    for items in data:
+        date = items['Tanggal_Terbit']
+        date = datetime.strptime(date, '%d-%m-%Y').date()
+        values = (items['Nomor_Registrasi'],
+                  date,
+                  items['Diterbitkan_Oleh'],
+                  items['Produk'],
+                  items['Nama_Produk'],
+                  items['Merk'],
+                  items['Kemasan'],
+                  items['Pendaftar'])
+        try:
+            mycursor.execute(sql, values)
+        except mysql.connector.errors.IntegrityError:
+            print("Duplicate")
+    mydb.commit()
+
+
+
+
